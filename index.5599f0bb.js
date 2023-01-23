74155c9254c103e5bab36c48ed9a7fb2b84db674
SergeLumiere <98837189+SergeLumiere@users.noreply.github.com>
2023-01-23 15:15:00 +0000

    Deploying to gh-pages from @ SergeLumiere/different-but-the-same@d35492a22afd206317a112df52bd8fcc4e1d8b81 🚀

---
+++ index.5599f0bb.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-about-modal-open]"),closeModalBtn:document.querySelector("[data-about-modal-close]"),modal:document.querySelector("[data-about-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.5599f0bb.js.map
--- index.9dd6afc9.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[about-data-modal-open]"),closeModalBtn:document.querySelector("[about-data-modal-close]"),modal:document.querySelector("[about-data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.9dd6afc9.js.map
