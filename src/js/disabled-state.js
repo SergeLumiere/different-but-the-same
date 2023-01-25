76aed5c4e46dd6e494200372c0c46b00d4594d25
SergeLumiere <sergemrcat@gmail.com>
2023-01-25 18:31:58 +0200

    Changed script for disabled btn

--- src/js/disabled-state.js
+++ src/js/disabled-state.js
@@ -1,5 +1,5 @@
-inp = document.querySelector('.inp');
-btnm = document.querySelector('.btnm');
+const inp = document.querySelector('.input');
+const btnm = document.querySelector('.button');
 // btnm.setAttribute('disabled', true);
 inp.oninput = function () {
   if (inp.value.length < 2) {
